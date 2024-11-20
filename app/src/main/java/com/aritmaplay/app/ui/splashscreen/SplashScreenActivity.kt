aea82750714d7286271a1a938e112d30cd71420e
Dayana-K-H <harahapdayana01@gmail.com>
2024-11-20 17:30:28 +0700

    feat: add splash screen activity transition

--- app/src/main/java/com/aritmaplay/app/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/aritmaplay/app/ui/splashscreen/SplashScreenActivity.kt
@@ -1,12 +1,18 @@
 package com.aritmaplay.app.ui.splashscreen
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.aritmaplay.app.MainActivity
 import com.aritmaplay.app.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +23,10 @@ class SplashScreenActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
     }
 }
