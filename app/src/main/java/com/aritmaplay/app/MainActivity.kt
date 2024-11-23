deb21238f8cd3115a9b11bbae11d6cd13b4187ca
Dayana-K-H <harahapdayana01@gmail.com>
2024-11-23 13:06:03 +0700

    chore: hide bottom navigation view in quiz fragment

--- app/src/main/java/com/aritmaplay/app/MainActivity.kt
+++ app/src/main/java/com/aritmaplay/app/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                R.id.historyFragment -> {
+                R.id.historyFragment, R.id.quizFragment -> {
                     binding.navView.visibility = View.GONE
                     supportActionBar?.show()
                 }
