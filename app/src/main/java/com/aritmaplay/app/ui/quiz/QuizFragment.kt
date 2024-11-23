521c95f8c3cc2387ce979ce9b593f0fc02a78f1a
Dayana-K-H <harahapdayana01@gmail.com>
2024-11-23 13:01:04 +0700

    feat: add logic for quiz fragment

--- app/src/main/java/com/aritmaplay/app/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/aritmaplay/app/ui/quiz/QuizFragment.kt
@@ -1,60 +1,70 @@
 package com.aritmaplay.app.ui.quiz
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.aritmaplay.app.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.aritmaplay.app.databinding.FragmentQuizBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [QuizFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class QuizFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentQuizBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var isCanvasEmpty = true
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+    ): View {
+        _binding = FragmentQuizBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment QuizFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            QuizFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initDrawView()
+
+        binding.sendButton.setOnClickListener {
+            if (isCanvasEmpty) {
+                Toast.makeText(requireContext(), "Canvas konsong!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Data terkirim!", Toast.LENGTH_SHORT).show()
+                binding.drawView.clearCanvas(needsSaving = false)
+                isCanvasEmpty = true
             }
+        }
+
+        binding.deleteButton.setOnClickListener {
+            binding.drawView.clearCanvas(needsSaving = false)
+            isCanvasEmpty = true
+            Toast.makeText(requireContext(), "Canvas cleared!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initDrawView() {
+        binding.drawView.apply {
+            brushSize = 55f
+            canvasBackgroundColor = Color.WHITE
+
+            drawViewPressCallback = {
+                Log.d("DRAW_VIEW", "Canvas touched")
+                isCanvasEmpty = false
+            }
+
+            undoStateCallback = {
+                Log.d("DRAW_VIEW", "Undo button pressed")
+                isCanvasEmpty = false
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
