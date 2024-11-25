f04a76008ca5f4bf2879c8959801f732941fef39
Dayana-K-H <harahapdayana01@gmail.com>
2024-11-25 10:12:06 +0700

    feat: add ViewModel for session handling and update UI initialization logic in login activity

--- app/src/main/java/com/aritmaplay/app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aritmaplay/app/ui/login/LoginActivity.kt
@@ -1,16 +1,24 @@
 package com.aritmaplay.app.ui.login
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.widget.Toast
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.aritmaplay.app.MainActivity
+import com.aritmaplay.app.ViewModelFactory
+import com.aritmaplay.app.data.pref.UserModel
 import com.aritmaplay.app.databinding.ActivityLoginBinding
 import com.aritmaplay.app.ui.signup.SignUpActivity
 
 class LoginActivity : AppCompatActivity() {
+    private val viewModel by viewModels<LoginViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
 
     private lateinit var binding: ActivityLoginBinding
 
@@ -20,13 +28,8 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupTextWatchers()
-
-        binding.loginButton.setOnClickListener {
-            val email = binding.edLoginEmail.text.toString()
-            val password = binding.edLoginPassword.text.toString()
-            Toast.makeText(this, "Email: $email\nPassword: $password", Toast.LENGTH_SHORT).show()
-            goToMainActivity()
-        }
+        setupView()
+        setupAction()
 
         binding.signupButton.setOnClickListener {
             goToSignUpActivity()
@@ -44,9 +47,27 @@ class LoginActivity : AppCompatActivity() {
         binding.edLoginPassword.addTextChangedListener(textWatcher)
     }
 
-    private fun goToMainActivity() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.loginButton.setOnClickListener {
+            viewModel.saveSession(UserModel("sample_token"))
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
+        }
     }
 
     private fun goToSignUpActivity() {
