a30a76f7afee4eea7bcaa4d4d1e80cdc1fd7fded
Dayana-K-H <harahapdayana01@gmail.com>
2024-11-25 10:06:54 +0700

    chore: add save session function

--- app/src/main/java/com/aritmaplay/app/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/aritmaplay/app/ui/login/LoginViewModel.kt
@@ -1,4 +1,15 @@
 package com.aritmaplay.app.ui.login
 
-class LoginViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aritmaplay.app.data.UserRepository
+import com.aritmaplay.app.data.pref.UserModel
+import kotlinx.coroutines.launch
+
+class LoginViewModel(private val repository: UserRepository) : ViewModel() {
+    fun saveSession(user: UserModel) {
+        viewModelScope.launch {
+            repository.saveSession(user)
+        }
+    }
 }
