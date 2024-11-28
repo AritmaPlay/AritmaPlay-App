241862581321614f4812d66d16f0828d058db738
Dayana-K-H <harahapdayana01@gmail.com>
2024-11-28 11:41:17 +0700

    feat: implement view model in SignUpActivity

--- app/src/main/java/com/aritmaplay/app/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/aritmaplay/app/ui/signup/SignUpActivity.kt
@@ -4,13 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import android.view.View
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.aritmaplay.app.R
+import com.aritmaplay.app.ViewModelFactory
 import com.aritmaplay.app.databinding.ActivitySignUpBinding
 import com.aritmaplay.app.ui.login.LoginActivity
+import com.aritmaplay.app.data.Result
 
 class SignUpActivity : AppCompatActivity() {
+    private val viewModel by viewModels<SignUpViewModel> {
+        ViewModelFactory.getInstance(this)
+    }
+
     private lateinit var binding: ActivitySignUpBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +29,30 @@ class SignUpActivity : AppCompatActivity() {
 
         setupAction()
         setupTextWatchers()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.signupResult.observe(this) { state ->
+            when (state) {
+                is Result.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    Log.d("SignUpActivity", "Loading user data...")
+                }
+
+                is Result.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    Log.d("SignUpActivity", "Success: User data fetched successfully!")
+                    goToLoginActivity()
+                }
+
+                is Result.Error -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(this, state.message, Toast.LENGTH_SHORT).show()
+                    Log.e("SignUpActivity", "Error: ${state.message}")
+                }
+            }
+        }
     }
 
     private fun setupAction() {
@@ -47,15 +80,14 @@ class SignUpActivity : AppCompatActivity() {
                 ).show()
                 return@setOnClickListener
             }
-            goToLoginActivity()
+            viewModel.register(name, userName, email, password)
         }
     }
 
     private fun setupTextWatchers() {
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
             override fun afterTextChanged(s: Editable?) {}
         }
 
