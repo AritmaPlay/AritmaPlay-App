c6fbc0cfa05afc3b90ed68326c28789d4dc9ddab
Dayana-K-H <harahapdayana01@gmail.com>
2024-11-28 11:40:30 +0700

    chore: user registration logic in SignUpViewModel

--- app/src/main/java/com/aritmaplay/app/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/aritmaplay/app/ui/signup/SignUpViewModel.kt
@@ -1,4 +1,28 @@
 package com.aritmaplay.app.ui.signup
 
-class SignUpViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aritmaplay.app.data.Result
+import com.aritmaplay.app.data.UserRepository
+import com.aritmaplay.app.data.response.RegisterResponse
+import kotlinx.coroutines.launch
+
+class SignUpViewModel(private val userRepository: UserRepository) : ViewModel() {
+
+    private val _signupResult = MutableLiveData<Result<RegisterResponse>>()
+    val signupResult: LiveData<Result<RegisterResponse>> = _signupResult
+
+    fun register(username: String, name: String, email: String, password: String) {
+        viewModelScope.launch {
+            _signupResult.value = Result.Loading
+            try {
+                val response = userRepository.register(username, name, email, password)
+                _signupResult.value = Result.Success(response)
+            } catch (e: Exception) {
+                _signupResult.value = Result.Error(e.toString())
+            }
+        }
+    }
 }
