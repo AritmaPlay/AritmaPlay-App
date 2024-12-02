382917d53cb270a43fb87fdc382fe70dcdf069ef
Dayana-K-H <harahapdayana01@gmail.com>
2024-12-02 22:35:44 +0700

    chore: remove unused import

--- app/src/main/java/com/aritmaplay/app/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aritmaplay/app/ui/login/LoginActivity.kt
@@ -1,14 +1,11 @@
 package com.aritmaplay.app.ui.login
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import android.view.WindowInsets
-import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
