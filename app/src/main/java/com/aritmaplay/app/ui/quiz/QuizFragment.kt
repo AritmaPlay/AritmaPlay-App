81ab4112a08984d20c0eb5fcbf40b8bb15cead9e
alvin afrinaldo <alvintodayafri@gmail.com>
2024-12-03 15:18:47 +0700

    chore: add and use viewmodel for quiz

--- app/src/main/java/com/aritmaplay/app/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/aritmaplay/app/ui/quiz/QuizFragment.kt
@@ -8,11 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.aritmaplay.app.R
+import com.aritmaplay.app.data.Result
+import com.aritmaplay.app.ViewModelFactory
 import com.aritmaplay.app.databinding.FragmentQuizBinding
+import kotlinx.coroutines.launch
 
 class QuizFragment : Fragment() {
+    private val viewModel by viewModels<QuizViewModel> {
+        ViewModelFactory.getInstance(requireContext())
+    }
+
     private var _binding: FragmentQuizBinding? = null
     private val binding get() = _binding!!
 
@@ -30,11 +39,15 @@ class QuizFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initDrawView()
+        observeViewModel()
 
         binding.sendButton.setOnClickListener {
             if (isCanvasEmpty) {
                 Toast.makeText(requireContext(), "Canvas konsong!", Toast.LENGTH_SHORT).show()
             } else {
+                val bitmap = binding.drawView.saveAsBitmap()
+                viewModel.predict(bitmap, requireContext())
+
                 Toast.makeText(requireContext(), "Data terkirim!", Toast.LENGTH_SHORT).show()
                 binding.drawView.clearCanvas(needsSaving = false)
                 isCanvasEmpty = true
@@ -47,6 +60,30 @@ class QuizFragment : Fragment() {
             binding.drawView.clearCanvas(needsSaving = false)
             isCanvasEmpty = true
             Toast.makeText(requireContext(), "Canvas cleared!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.predictResult.observe(requireActivity()) { state ->
+            when (state) {
+                is Result.Loading -> {
+                    binding.progressBar.visibility = View.VISIBLE
+                    Log.d("HandwritingPredict", "Predicting...")
+                }
+
+                is Result.Success -> {
+                    binding.progressBar.visibility = View.GONE
+                    Log.d("HandwritingPredict", "Predict: ${state.data.data}")
+                    lifecycleScope.launch {
+                    }
+                }
+
+                is Result.Error -> {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(requireContext(), state.message, Toast.LENGTH_SHORT).show()
+                    Log.e("HandwritingPredict", "Error: ${state.message}")
+                }
+            }
         }
     }
 
