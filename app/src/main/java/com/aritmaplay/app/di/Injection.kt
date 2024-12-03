a0a4bf929e0f3581f3553277c00e59e3271de051
alvin afrinaldo <alvintodayafri@gmail.com>
2024-12-03 15:18:16 +0700

    chore: add provide quiz repository

--- app/src/main/java/com/aritmaplay/app/di/Injection.kt
+++ app/src/main/java/com/aritmaplay/app/di/Injection.kt
@@ -1,15 +1,24 @@
 package com.aritmaplay.app.di
 
 import android.content.Context
-import com.aritmaplay.app.data.UserRepository
-import com.aritmaplay.app.data.pref.UserPreference
-import com.aritmaplay.app.data.pref.dataStore
-import com.aritmaplay.app.data.retrofit.UserApiConfig
+import com.aritmaplay.app.repository.UserRepository
+import com.aritmaplay.app.data.local.pref.UserPreference
+import com.aritmaplay.app.data.local.pref.dataStore
+import com.aritmaplay.app.data.remote.retrofit.handwriting.HandwritingPredictApiConfig
+import com.aritmaplay.app.data.remote.retrofit.user.UserApiConfig
+import com.aritmaplay.app.repository.QuizRepository
 
 object Injection {
-    fun provideRepository(context: Context): UserRepository {
+
+    fun provideUserRepository(context: Context): UserRepository {
         val pref = UserPreference.getInstance(context.dataStore)
         val apiService = UserApiConfig.getApiService()
         return UserRepository.getInstance(pref, apiService)
+    }
+
+    fun provideQuizRepository(): QuizRepository {
+        val handwritingPredictApiService = HandwritingPredictApiConfig.getHandwritingPredictApiService()
+        val userApiService = UserApiConfig.getApiService()
+        return QuizRepository.getInstance(userApiService, handwritingPredictApiService)
     }
 }
