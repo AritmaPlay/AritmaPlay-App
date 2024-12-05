ed8efd7de245ed76b81be86e75d90ecc4fa309ab
alvin afrinaldo <alvintodayafri@gmail.com>
2024-12-05 21:14:41 +0700

    chore: increase duration of splashscreen

--- app/src/main/java/com/aritmaplay/app/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/aritmaplay/app/ui/splashscreen/SplashScreenActivity.kt
@@ -3,6 +3,8 @@ package com.aritmaplay.app.ui.splashscreen
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -24,6 +26,7 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         splashScreen.setKeepOnScreenCondition{ true }
+        Handler(Looper.getMainLooper()).postDelayed({}, 1000)
 
         val userPreference = UserPreference.getInstance(dataStore)
 
