9c413e661aebbf13892412de78618aed96bb5f97
alvin afrinaldo <alvintodayafri@gmail.com>
2024-12-05 15:49:57 +0700

    chore: use converter gson for response

--- app/src/main/java/com/aritmaplay/app/data/remote/response/user/LoginResponse.kt
+++ app/src/main/java/com/aritmaplay/app/data/remote/response/user/LoginResponse.kt
@@ -1,12 +1,55 @@
 package com.aritmaplay.app.data.remote.response.user
 
+import com.google.gson.annotations.SerializedName
+
 data class LoginResponse(
-    val success: Boolean,
-    val message: String,
-    val response_code: Int,
-    val data: Data
+    @field:SerializedName("response_code")
+    val responseCode: Int? = null,
+
+    @field:SerializedName("data")
+    val data: Data? = null,
+
+    @field:SerializedName("success")
+    val success: Boolean? = null,
+
+    @field:SerializedName("message")
+    val message: String? = null
+)
+
+data class User(
+
+    @field:SerializedName("updated_at")
+    val updatedAt: String? = null,
+
+    @field:SerializedName("user_id")
+    val userId: Int? = null,
+
+    @field:SerializedName("level")
+    val level: Int? = null,
+
+    @field:SerializedName("name")
+    val name: String? = null,
+
+    @field:SerializedName("created_at")
+    val createdAt: String? = null,
+
+    @field:SerializedName("urlProfile")
+    val urlProfile: Any? = null,
+
+    @field:SerializedName("email")
+    val email: String? = null,
+
+    @field:SerializedName("totalExp")
+    val totalExp: Int? = null,
+
+    @field:SerializedName("username")
+    val username: String? = null
 )
 
 data class Data(
-    val token: String
+    @field:SerializedName("user")
+    val user: User? = null,
+
+    @field:SerializedName("token")
+    val token: String? = null
 )
--- app/src/main/java/com/aritmaplay/app/data/remote/response/user/RegisterResponse.kt
+++ app/src/main/java/com/aritmaplay/app/data/remote/response/user/RegisterResponse.kt
@@ -1,16 +1,34 @@
 package com.aritmaplay.app.data.remote.response.user
 
+import com.google.gson.annotations.SerializedName
+
 data class RegisterResponse(
-    val success: Boolean,
-    val message: String,
-    val response_code: Int,
-    val data: RegisterData
+    @field:SerializedName("response_code")
+    val responseCode: Int? = null,
+
+    @field:SerializedName("data")
+    val data: RegisterData? = null,
+
+    @field:SerializedName("success")
+    val success: Boolean? = null,
+
+    @field:SerializedName("message")
+    val message: String? = null
 )
 
 data class RegisterData(
-    val username: String,
-    val email: String,
-    val updated_at: String,
-    val created_at: String,
-    val id: Int
+    @field:SerializedName("updated_at")
+    val updatedAt: String? = null,
+
+    @field:SerializedName("created_at")
+    val createdAt: String? = null,
+
+    @field:SerializedName("id")
+    val id: Int? = null,
+
+    @field:SerializedName("email")
+    val email: String? = null,
+
+    @field:SerializedName("username")
+    val username: String? = null
 )
