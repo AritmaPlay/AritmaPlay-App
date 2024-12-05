30f45ed3c202f4e43991280c4cb1c7cf40d5289d
alvin afrinaldo <alvintodayafri@gmail.com>
2024-12-05 17:33:50 +0700

    chore: add provideProfileRepository

--- app/src/main/java/com/aritmaplay/app/di/Injection.kt
+++ app/src/main/java/com/aritmaplay/app/di/Injection.kt
@@ -5,8 +5,12 @@ import com.aritmaplay.app.repository.UserRepository
 import com.aritmaplay.app.data.local.pref.UserPreference
 import com.aritmaplay.app.data.local.pref.dataStore
 import com.aritmaplay.app.data.remote.retrofit.handwriting.HandwritingPredictApiConfig
+import com.aritmaplay.app.data.remote.retrofit.profile.ProfileApiConfig
 import com.aritmaplay.app.data.remote.retrofit.user.UserApiConfig
+import com.aritmaplay.app.repository.ProfileRepository
 import com.aritmaplay.app.repository.QuizRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 object Injection {
 
@@ -20,5 +24,12 @@ object Injection {
         val handwritingPredictApiService = HandwritingPredictApiConfig.getHandwritingPredictApiService()
         val userApiService = UserApiConfig.getApiService()
         return QuizRepository.getInstance(userApiService, handwritingPredictApiService)
+    }
+
+    fun provideProfileRepository(context: Context): ProfileRepository {
+        val pref = UserPreference.getInstance(context.dataStore)
+        val user = runBlocking { pref.getSession().first() }
+        val apiService = ProfileApiConfig.getApiService(user.token)
+        return ProfileRepository.getInstance(pref, apiService)
     }
 }
