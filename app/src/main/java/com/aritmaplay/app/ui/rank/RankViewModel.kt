f1d0bff11bc05cda733748997e8cce3d5033066f
Dayana-K-H <harahapdayana01@gmail.com>
2024-12-06 17:21:05 +0700

    chore: change RankViewModel to integrate Leaderboard API

--- app/src/main/java/com/aritmaplay/app/ui/rank/RankViewModel.kt
+++ app/src/main/java/com/aritmaplay/app/ui/rank/RankViewModel.kt
@@ -3,19 +3,27 @@ package com.aritmaplay.app.ui.rank
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aritmaplay.app.data.Result
+import com.aritmaplay.app.data.remote.response.leaderboard.LeaderboardResponse
+import com.aritmaplay.app.repository.LeaderboardRepository
+import kotlinx.coroutines.launch
 
-class RankViewModel : ViewModel() {
+class RankViewModel (private val repository: LeaderboardRepository) : ViewModel() {
 
-    private val _rankList = MutableLiveData<List<RankItem>>()
-    val rankList: LiveData<List<RankItem>> = _rankList
+    private val _leaderboardList = MutableLiveData<Result<LeaderboardResponse>>()
+    val leaderboardList: LiveData<Result<LeaderboardResponse>> = _leaderboardList
 
-    init {
-        _rankList.value = listOf(
-            RankItem("Alice", 150),
-            RankItem("Bob", 200),
-            RankItem("Charlie", 120),
-            RankItem("Diana", 180),
-            RankItem("Eve", 170)
-        ).sortedByDescending { it.xp }
+
+    fun getLeaderboard(token: String) {
+        viewModelScope.launch {
+            _leaderboardList.value = Result.Loading
+            try {
+                val response = repository.getLeaderboard(token)
+                _leaderboardList.value = Result.Success(response)
+            } catch (e: Exception) {
+                _leaderboardList.value = Result.Error(e.toString())
+            }
+        }
     }
 }
