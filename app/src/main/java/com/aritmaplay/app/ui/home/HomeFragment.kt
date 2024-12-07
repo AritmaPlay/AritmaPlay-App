90bbfa85a4e5871ef3585bd10069b0bf7bf59b9d
alvin afrinaldo <alvintodayafri@gmail.com>
2024-12-07 17:39:10 +0700

    chore: handle back

--- app/src/main/java/com/aritmaplay/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/aritmaplay/app/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
@@ -66,6 +67,12 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        })
 
         val userPreference = UserPreference.getInstance(requireContext().dataStore)
 
