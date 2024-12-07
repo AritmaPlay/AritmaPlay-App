6ca98a37d14bb26cd5cbc4042e54db0b926668b3
Dayana-K-H <harahapdayana01@gmail.com>
2024-12-07 16:37:37 +0700

    chore: add provideQuizResultRepository in Injection

--- app/src/main/java/com/aritmaplay/app/di/Injection.kt
+++ app/src/main/java/com/aritmaplay/app/di/Injection.kt
@@ -11,11 +11,11 @@ import com.aritmaplay.app.repository.HistoryRepository
 import com.aritmaplay.app.repository.LeaderboardRepository
 import com.aritmaplay.app.repository.ProfileRepository
 import com.aritmaplay.app.repository.QuizRepository
+import com.aritmaplay.app.repository.ResultRepository
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 
 object Injection {
-
     fun provideUserRepository(context: Context): UserRepository {
         val pref = UserPreference.getInstance(context.dataStore)
         val apiService = UserApiConfig.getApiService()
@@ -47,5 +47,12 @@ object Injection {
         val user = runBlocking { pref.getSession().first() }
         val apiService = ProfileApiConfig.getApiService(user.token)
         return HistoryRepository.getInstance(apiService)
+    }
+
+    fun provideQuizResultRepository(context: Context): ResultRepository {
+        val pref = UserPreference.getInstance(context.dataStore)
+        val user = runBlocking { pref.getSession().first() }
+        val apiService = ProfileApiConfig.getApiService(user.token)
+        return ResultRepository.getInstance(apiService)
     }
 }
