8469489d6167d74c6d97b7ad4ae953978198a386
Dayana-K-H <harahapdayana01@gmail.com>
2024-12-08 20:33:52 +0700

    feat: implement adapter in WeeklyFragment and AllTimeFragment

--- app/src/main/java/com/aritmaplay/app/ui/rank/WeeklyFragment.kt
+++ app/src/main/java/com/aritmaplay/app/ui/rank/WeeklyFragment.kt
@@ -1,60 +1,140 @@
 package com.aritmaplay.app.ui.rank
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.aritmaplay.app.data.Result
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.aritmaplay.app.R
+import com.aritmaplay.app.SpaceItemDecoration
+import com.aritmaplay.app.ViewModelFactory
+import com.aritmaplay.app.data.local.pref.UserPreference
+import com.aritmaplay.app.data.local.pref.dataStore
+import com.aritmaplay.app.databinding.FragmentWeeklyBinding
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WeeklyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WeeklyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private val viewModel by viewModels<RankViewModel> {
+        ViewModelFactory.getInstance(requireContext())
     }
+
+    private var _binding: FragmentWeeklyBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var weeklyAdapter: WeeklyAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weekly, container, false)
+    ): View {
+        _binding = FragmentWeeklyBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WeeklyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WeeklyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val userPreference = UserPreference.getInstance(requireContext().dataStore)
+
+        lifecycleScope.launch {
+            userPreference.getSession().collect { user ->
+                if (user.isLogin) {
+                    viewModel.getWeeklyLeaderboard("Bearer ${user.token}")
+                } else {
+                    Toast.makeText(context, "Silakan login terlebih dahulu", Toast.LENGTH_SHORT).show()
                 }
             }
+        }
+
+        val spaceInPixels = resources.getDimensionPixelSize(R.dimen.space_between_items)
+        binding.rvRank.addItemDecoration(SpaceItemDecoration(spaceInPixels))
+        weeklyAdapter = WeeklyAdapter()
+
+        viewModel.weeklyLeaderboardList.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is Result.Loading -> {
+                    Log.d("RankFragment", "Loading state: Memuat data leaderboard.")
+                    binding.progressBar.visibility = View.VISIBLE
+                    binding.rvRank.visibility = View.GONE
+                    binding.tvNoData.visibility = View.GONE
+                    binding.tvExpRank.visibility = View.GONE
+                    binding.tvNameRank.visibility = View.GONE
+                }
+                is Result.Success -> {
+                    Log.d("RankFragment", "Success state: Data leaderboard diterima.")
+                    val rankList= result.data.data?.leaderboardEntries
+                    if (rankList != null) {
+                        Log.d("RankFragment", "Jumlah entries diterima: ${rankList.size}")
+                        if (rankList.isEmpty()) {
+                            Log.d("RankFragment", "Entries kosong: Menampilkan pesan 'Tidak ada data'.")
+                            binding.rvRank.visibility = View.GONE
+                            binding.tvNoData.visibility = View.VISIBLE
+                        } else {
+                            Log.d("RankFragment", "Entries tersedia: Menampilkan RecyclerView.")
+                            binding.rvRank.visibility = View.VISIBLE
+                            binding.tvNoData.visibility = View.GONE
+                            binding.tvExpRank.visibility = View.VISIBLE
+                            binding.tvNameRank.visibility = View.VISIBLE
+                            val rankedList = rankList.mapIndexed { index, item ->
+                                item?.copy(rank = index + 1)
+                            }
+                            weeklyAdapter.submitList(rankedList)
+
+                            lifecycleScope.launch {
+                                val user = userPreference.getSession().firstOrNull()
+                                val LoginUser = user?.let {
+                                    rankedList.find { it?.userId == user.userId }
+                                }
+                                if (LoginUser == null) {
+                                    binding.tvNumberListRank.text = getString(R.string.list_number_rank)
+                                    binding.tvExpRank.visibility = View.GONE
+                                    binding.tvNameRank.text = getString(R.string.first_time_user)
+
+                                } else {
+                                    binding.tvExpRank.visibility = View.VISIBLE
+                                    binding.tvNumberListRank.text = LoginUser.rank.toString()
+                                    binding.tvNameRank.text = LoginUser.user?.name?.let {
+                                        it.replaceFirstChar { char ->
+                                            if (char.isLowerCase()) char.titlecase() else char.toString()
+                                        }
+                                    }.orEmpty()
+                                    binding.tvExpRank.text = "${LoginUser.totalExpPerWeek} EXP"
+                                }
+                            }
+                        }
+                    } else {
+                        Log.w("RankFragment", "Data entries null. Tidak ada data untuk ditampilkan.")
+                        binding.rvRank.visibility = View.GONE
+                        binding.tvNoData.visibility = View.VISIBLE
+                    }
+                    binding.progressBar.visibility = View.GONE
+                }
+                is Result.Error -> {
+                    Log.e("RankFragment", "Error state: ${result.message}")
+                    binding.progressBar.visibility = View.GONE
+                    binding.rvRank.visibility = View.GONE
+                    binding.tvNoData.visibility = View.VISIBLE
+                    Toast.makeText(requireContext(), "Error loading data", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        binding.rvRank.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            setHasFixedSize(true)
+            adapter = weeklyAdapter
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
