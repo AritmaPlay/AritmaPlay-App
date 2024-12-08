3be4f15de8caaf5907e5f26eac3734d4ce6c38ab
Dayana-K-H <harahapdayana01@gmail.com>
2024-12-08 20:30:02 +0700

    chore: add getAllTimeLeaderboard fun in RankViewModel

--- app/src/main/java/com/aritmaplay/app/ui/rank/RankViewModel.kt
+++ app/src/main/java/com/aritmaplay/app/ui/rank/RankViewModel.kt
@@ -5,24 +5,39 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.aritmaplay.app.data.Result
-import com.aritmaplay.app.data.remote.response.leaderboard.LeaderboardResponse
+import com.aritmaplay.app.data.remote.response.leaderboard.AllTimeLeaderboardResponse
+import com.aritmaplay.app.data.remote.response.leaderboard.WeeklyLeaderboardResponse
 import com.aritmaplay.app.repository.LeaderboardRepository
 import kotlinx.coroutines.launch
 
 class RankViewModel (private val repository: LeaderboardRepository) : ViewModel() {
 
-    private val _leaderboardList = MutableLiveData<Result<LeaderboardResponse>>()
-    val leaderboardList: LiveData<Result<LeaderboardResponse>> = _leaderboardList
+    private val _weeklyLeaderboardList = MutableLiveData<Result<WeeklyLeaderboardResponse>>()
+    val weeklyLeaderboardList: LiveData<Result<WeeklyLeaderboardResponse>> = _weeklyLeaderboardList
 
+    private val _allTimeLeaderboardList = MutableLiveData<Result<AllTimeLeaderboardResponse>>()
+    val allTimeLeaderboardList: LiveData<Result<AllTimeLeaderboardResponse>> = _allTimeLeaderboardList
 
-    fun getLeaderboard(token: String) {
+    fun getWeeklyLeaderboard(token: String) {
         viewModelScope.launch {
-            _leaderboardList.value = Result.Loading
+            _weeklyLeaderboardList.value = Result.Loading
             try {
-                val response = repository.getLeaderboard(token)
-                _leaderboardList.value = Result.Success(response)
+                val response = repository.getWeeklyLeaderboard(token)
+                _weeklyLeaderboardList.value = Result.Success(response)
             } catch (e: Exception) {
-                _leaderboardList.value = Result.Error(e.toString())
+                _weeklyLeaderboardList.value = Result.Error(e.toString())
+            }
+        }
+    }
+
+    fun getAllTimeLeaderboard(token: String) {
+        viewModelScope.launch {
+            _allTimeLeaderboardList.value = Result.Loading
+            try {
+                val response = repository.getAllTimeLeaderboard(token)
+                _allTimeLeaderboardList.value = Result.Success(response)
+            } catch (e: Exception) {
+                _allTimeLeaderboardList.value = Result.Error(e.toString())
             }
         }
     }
